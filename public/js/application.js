88e6c457d68da294a688a6102e7f33cf0d2d6ea9
Lynne Tye <lynnetye@gmail.com>
2015-04-21 12:37:53 -0700

    second attempt to add google plus signin

--- public/js/application.js
+++ public/js/application.js
@@ -44,44 +44,4 @@ $(document).ready(function() {
     })
   })
 
-    /* Executed when the APIs finish loading */
-    function render() {
-
-     // Additional params including the callback, the rest of the params will
-     // come from the page-level configuration.
-     var additionalParams = {
-       'callback': signinCallback
-     };
-
-     // TEST
-     $("body").on("click", function(){
-        alert("yay");
-     });
-
-     // Attach a click listener to a button to trigger the flow.
-     // var signinButton = document.getElementById('signinButton');
-     // signinButton.addEventListener('click', function() {
-      $("#signinButton").on("click", function(event){
-        event.preventDefault();
-        $(".nav-buttons > a").css({"color": "red"});
-        debugger
-       // gapi.auth.signIn(additionalParams); // Will use page level configuration
-     });
-   }
-
-  function signinCallback(authResult) {
-    if (authResult['status']['signed_in']) {
-      // Update the app to reflect a signed in user
-      // Hide the sign-in button now that the user is authorized, for example:
-      document.getElementById('signinButton').setAttribute('style', 'display: none');
-    } else {
-      // Update the app to reflect a signed out user
-      // Possible error values:
-      //   "user_signed_out" - User is signed-out
-      //   "access_denied" - User denied access to your app
-      //   "immediate_failed" - Could not automatically log in the user
-      console.log('Sign-in state: ' + authResult['error']);
-    }
-  }
-
 });
---
+++ public/js/google-plus-signin-api.js
@@ -0,0 +1,56 @@
+function logout()
+{
+    gapi.auth.signOut();
+    location.reload();
+}
+function login()
+{
+  var myParams = {
+    'clientid' : ENV["CLIENT_ID"],
+    'cookiepolicy' : 'single_host_origin',
+    'callback' : 'loginCallback',
+    'approvalprompt':'force',
+    'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+  };
+  gapi.auth.signIn(myParams);
+}
+
+function loginCallback(result)
+{
+    if(result['status']['signed_in'])
+    {
+        var request = gapi.client.plus.people.get(
+        {
+            'userId': 'me'
+        });
+        request.execute(function (resp)
+        {
+            var email = '';
+            if(resp['emails'])
+            {
+                for(i = 0; i < resp['emails'].length; i++)
+                {
+                    if(resp['emails'][i]['type'] == 'account')
+                    {
+                        email = resp['emails'][i]['value'];
+                    }
+                }
+            }
+
+            var str = "Name:" + resp['displayName'] + "<br>";
+            str += "Image:" + resp['image']['url'] + "<br>";
+            str += "<img src='" + resp['image']['url'] + "' /><br>";
+
+            str += "URL:" + resp['url'] + "<br>";
+            str += "Email:" + email + "<br>";
+            document.getElementById("profile").innerHTML = str;
+        });
+
+    }
+
+}
+function onLoadCallback()
+{
+    gapi.client.setApiKey(ENV["SECRET_KEY"]);
+    gapi.client.load('plus', 'v1',function(){});
+}
